605833e1d870190db10a53682a77f548c6c37b77
Ramadan Ibrahem <61190974+RamadanIbrahem98@users.noreply.github.com>
2021-03-25 15:25:36 +0200

    Add the base template design

---
+++ UI/main.py
@@ -0,0 +1,22 @@
+import sys
+from PyQt5 import QtWidgets as qtw
+from PyQt5 import QtCore as qtc
+from PyQt5 import QtGui as qtg
+from PyQt5 import uic
+
+from layout import Ui_MainWindow
+
+
+class MainWindow(qtw.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.show()
+
+
+if __name__ == '__main__':
+    app = qtw.QApplication(sys.argv)
+    mw = MainWindow()
+    sys.exit(app.exec_())
