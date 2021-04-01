908bfafaa87447431be0e223ffbc4a646e147da0
Ramadan Ibrahem <61190974+RamadanIbrahem98@users.noreply.github.com>
2021-04-01 13:05:39 +0200

    changed spectrogram axis

--- UI/main.py
+++ UI/main.py
@@ -1,4 +1,5 @@
-import sys, csv
+import sys
+import csv
 import numpy as np
 import pyqtgraph as pg
 import pyqtgraph.exporters
@@ -43,24 +44,32 @@ class MainWindow(qtw.QMainWindow):
         self.pointsToAppend = [0, 0, 0]
         self.isResumed = [False, False, False]
 
-
         # GUI Components to Manipulate
-        self.graphChannels = [self.ui.signal1Graph, self.ui.signal2Graph, self.ui.signal3Graph]
-        self.spectrogramChannels = [self.ui.spectrogram1Graph, self.ui.spectrogram2Graph, self.ui.spectrogram3Graph]
+        self.graphChannels = [self.ui.signal1Graph,
+                              self.ui.signal2Graph, self.ui.signal3Graph]
+        self.spectrogramChannels = [
+            self.ui.spectrogram1Graph, self.ui.spectrogram2Graph, self.ui.spectrogram3Graph]
         self.timers = [self.ui.timer1, self.ui.timer2, self.ui.timer3]
-        self.setChannelChecked = [self.ui.showChannel1, self.ui.showChannel2, self.ui.showChannel3]
-        self.channelComponents = [self.ui.channel1, self.ui.channel2, self.ui.channel3]
-        
+        self.setChannelChecked = [self.ui.showChannel1,
+                                  self.ui.showChannel2, self.ui.showChannel3]
+        self.channelComponents = [self.ui.channel1,
+                                  self.ui.channel2, self.ui.channel3]
 
         # GUI Compenents to be Triggered or Clicked
-        self.openChannels = [self.ui.actionOpenChannel1, self.ui.actionOpenChannel2, self.ui.actionOpenChannel3]
+        self.openChannels = [self.ui.actionOpenChannel1,
+                             self.ui.actionOpenChannel2, self.ui.actionOpenChannel3]
         self.playBtns = [self.ui.playBtn1, self.ui.playBtn2, self.ui.playBtn3]
-        self.pauseBtns = [self.ui.pauseBtn1, self.ui.pauseBtn2, self.ui.pauseBtn3]
-        self.autoRanges = [self.ui.focusBtn1, self.ui.focusBtn2, self.ui.focusBtn3]
-        self.zoomIns = [self.ui.zoomInBtn1, self.ui.zoomInBtn2, self.ui.zoomInBtn3]
-        self.zoomOuts = [self.ui.zoomOutBtn1, self.ui.zoomOutBtn2, self.ui.zoomOutBtn3]
-        self.clearGraphs = [self.ui.clearBtn1, self.ui.clearBtn2, self.ui.clearBtn3]
-        
+        self.pauseBtns = [self.ui.pauseBtn1,
+                          self.ui.pauseBtn2, self.ui.pauseBtn3]
+        self.autoRanges = [self.ui.focusBtn1,
+                           self.ui.focusBtn2, self.ui.focusBtn3]
+        self.zoomIns = [self.ui.zoomInBtn1,
+                        self.ui.zoomInBtn2, self.ui.zoomInBtn3]
+        self.zoomOuts = [self.ui.zoomOutBtn1,
+                         self.ui.zoomOutBtn2, self.ui.zoomOutBtn3]
+        self.clearGraphs = [self.ui.clearBtn1,
+                            self.ui.clearBtn2, self.ui.clearBtn3]
+
         # The pen variable corresponding with each plot
         self.pen = [pg.mkPen(color=(255, 0, 0), width=2), pg.mkPen(
             color=(0, 255, 0), width=2), pg.mkPen(color=(0, 0, 255), width=2)]
@@ -68,28 +77,34 @@ class MainWindow(qtw.QMainWindow):
         def mappingSlotsAndSignals(connections):
             for connection in connections:
                 connection
-        
+
         for channel in self.channelComponents:
             channel.show()
         for channelToCheck in self.setChannelChecked:
             channelToCheck.setChecked(True)
-        
 
-        mappingSlotsAndSignals(map(lambda showGraph, CHANNEL: showGraph.stateChanged.connect(lambda: self.hide(CHANNEL)), self.setChannelChecked, [self.CHANNEL1, self.CHANNEL2, self.CHANNEL3]))
-        mappingSlotsAndSignals(map(lambda open, CHANNEL: open.triggered.connect(lambda: self.browse(CHANNEL)), self.openChannels, [self.CHANNEL1, self.CHANNEL2, self.CHANNEL3]))
-        mappingSlotsAndSignals(map(lambda playBtn, CHANNEL: playBtn.clicked.connect(lambda: self.play(CHANNEL)), self.playBtns, [self.CHANNEL1, self.CHANNEL2, self.CHANNEL3]))
-        mappingSlotsAndSignals(map(lambda pauseBtn, CHANNEL: pauseBtn.clicked.connect(lambda: self.pause(CHANNEL)), self.pauseBtns, [self.CHANNEL1, self.CHANNEL2, self.CHANNEL3]))
-        mappingSlotsAndSignals(map(lambda autoRange, CHANNEL: autoRange.clicked.connect(lambda: self.graphChannels[CHANNEL].getPlotItem().enableAutoRange()), self.autoRanges, [self.CHANNEL1, self.CHANNEL2, self.CHANNEL3]))
-        mappingSlotsAndSignals(map(lambda zoomIn, CHANNEL: zoomIn.clicked.connect(lambda: self.zoomin(CHANNEL)), self.zoomIns, [self.CHANNEL1, self.CHANNEL2, self.CHANNEL3]))
-        mappingSlotsAndSignals(map(lambda zoomOut, CHANNEL: zoomOut.clicked.connect(lambda: self.zoomout(CHANNEL)), self.zoomOuts, [self.CHANNEL1, self.CHANNEL2, self.CHANNEL3]))
-        mappingSlotsAndSignals(map(lambda clearGraph, CHANNEL: clearGraph.clicked.connect(lambda: self.clear(CHANNEL)), self.clearGraphs, [self.CHANNEL1, self.CHANNEL2, self.CHANNEL3]))
+        mappingSlotsAndSignals(map(lambda showGraph, CHANNEL: showGraph.stateChanged.connect(
+            lambda: self.hide(CHANNEL)), self.setChannelChecked, [self.CHANNEL1, self.CHANNEL2, self.CHANNEL3]))
+        mappingSlotsAndSignals(map(lambda open, CHANNEL: open.triggered.connect(lambda: self.browse(
+            CHANNEL)), self.openChannels, [self.CHANNEL1, self.CHANNEL2, self.CHANNEL3]))
+        mappingSlotsAndSignals(map(lambda playBtn, CHANNEL: playBtn.clicked.connect(
+            lambda: self.play(CHANNEL)), self.playBtns, [self.CHANNEL1, self.CHANNEL2, self.CHANNEL3]))
+        mappingSlotsAndSignals(map(lambda pauseBtn, CHANNEL: pauseBtn.clicked.connect(
+            lambda: self.pause(CHANNEL)), self.pauseBtns, [self.CHANNEL1, self.CHANNEL2, self.CHANNEL3]))
+        mappingSlotsAndSignals(map(lambda autoRange, CHANNEL: autoRange.clicked.connect(
+            lambda: self.graphChannels[CHANNEL].getPlotItem().enableAutoRange()), self.autoRanges, [self.CHANNEL1, self.CHANNEL2, self.CHANNEL3]))
+        mappingSlotsAndSignals(map(lambda zoomIn, CHANNEL: zoomIn.clicked.connect(
+            lambda: self.zoomin(CHANNEL)), self.zoomIns, [self.CHANNEL1, self.CHANNEL2, self.CHANNEL3]))
+        mappingSlotsAndSignals(map(lambda zoomOut, CHANNEL: zoomOut.clicked.connect(
+            lambda: self.zoomout(CHANNEL)), self.zoomOuts, [self.CHANNEL1, self.CHANNEL2, self.CHANNEL3]))
+        mappingSlotsAndSignals(map(lambda clearGraph, CHANNEL: clearGraph.clicked.connect(
+            lambda: self.clear(CHANNEL)), self.clearGraphs, [self.CHANNEL1, self.CHANNEL2, self.CHANNEL3]))
         self.ui.actionAbout.triggered.connect(lambda: self.showAbout())
         self.ui.actionExit.triggered.connect(lambda: self.close())
         self.ui.actionNew.triggered.connect(lambda: self.create_new_window())
         self.ui.generatePDF.clicked.connect(lambda: self.generatePDF())
 
         self.show()
-
 
     def create_new_window(self):
         self.newWindow = MainWindow()
@@ -114,7 +129,8 @@ class MainWindow(qtw.QMainWindow):
         self.hide(channel)
 
         # self.clear(channel)
-        self.filenames[channel] = qtw.QFileDialog.getOpenFileName(None, 'Load Signal', './', "Raw Data(*.csv)")
+        self.filenames[channel] = qtw.QFileDialog.getOpenFileName(
+            None, 'Load Signal', './', "Raw Data(*.csv)")
         path = self.filenames[channel][0]
         self.openFile(path, channel)
 
@@ -152,7 +168,8 @@ class MainWindow(qtw.QMainWindow):
     def clear(self, channel: int) -> None:
         if self.y[channel]:
             self.graphChannels[channel].removeItem(self.data_line[channel])
-            self.spectrogramChannels[channel].removeItem(self.spectrogramData[channel])
+            self.spectrogramChannels[channel].removeItem(
+                self.spectrogramData[channel])
             self.timers[channel].stop()
             self.isResumed[channel] = False
             self.y[channel] = []
@@ -160,16 +177,17 @@ class MainWindow(qtw.QMainWindow):
             self.data_line[channel] = []
 
     def plotGraph(self, channel: int) -> None:
-        self.data_line[channel] = self.graphChannels[channel].plot(self.x[channel], self.y[channel], name='CH1', pen=self.pen[channel])
-        self.graphChannels[channel].plotItem.setLimits(xMin=0, xMax=6, yMin=-(np.mean(self.y[channel]) - min(self.y[channel])), yMax=np.mean(self.y[channel]) - min(self.y[channel]))
-        
+        self.data_line[channel] = self.graphChannels[channel].plot(
+            self.x[channel], self.y[channel], name='CH1', pen=self.pen[channel])
+        self.graphChannels[channel].plotItem.setLimits(xMin=0, xMax=6, yMin=-(np.mean(
+            self.y[channel]) - min(self.y[channel])), yMax=np.mean(self.y[channel]) - min(self.y[channel]))
+
         self.pointsToAppend[channel] = 0
         print()
         self.timers[channel].setInterval(150)
         self.timers[channel].timeout.connect(lambda: self.updatePlot(channel))
         if self.pointsToAppend[channel] < len(self.x[channel]):
             self.timers[channel].start()
-        
 
     def updatePlot(self, channel: int) -> None:
         xaxis = self.x[channel][:self.pointsToAppend[channel]]
@@ -180,22 +198,27 @@ class MainWindow(qtw.QMainWindow):
             self.timers[channel].stop()
 
         if self.x[channel][self.pointsToAppend[channel]] > 1.0:
-            self.graphChannels[channel].setLimits(xMin=min(xaxis, default=0), xMax=max(xaxis, default=0))
-        self.graphChannels[channel].plotItem.setXRange(max(xaxis, default=0)-1.0, max(xaxis, default=0))
+            self.graphChannels[channel].setLimits(
+                xMin=min(xaxis, default=0), xMax=max(xaxis, default=0))
+        self.graphChannels[channel].plotItem.setXRange(
+            max(xaxis, default=0)-1.0, max(xaxis, default=0))
 
         self.data_line[channel].setData(xaxis, yaxis)
 
     def plotSpectrogram(self, channel: int) -> None:
+        pyqtgraph.setConfigOptions(imageAxisOrder='row-major')
         fs = 1 / (self.x[channel][1] - self.x[channel][0])
         yaxis = np.array(self.y[channel])
         f, t, Sxx = scipy.signal.spectrogram(yaxis, fs)
-        self.spectrogramData[channel] = self.spectrogramChannels[channel].addPlot()
+        self.spectrogramData[channel] = self.spectrogramChannels[channel].addPlot(
+        )
 
         img = pg.ImageItem()
         self.spectrogramData[channel].addItem(img)
         hist = pg.HistogramLUTItem()
         hist.setImageItem(img)
-        self.spectrogramChannels[channel].addItem(self.spectrogramData[channel])
+        self.spectrogramChannels[channel].addItem(
+            self.spectrogramData[channel])
         self.spectrogramChannels[channel].show()
         hist.setLevels(np.min(Sxx), np.max(Sxx))
         hist.gradient.restoreState(
@@ -204,17 +227,19 @@ class MainWindow(qtw.QMainWindow):
                        (1.0, (246, 111, 0, 255)),
                        (0.0, (75, 0, 113, 255))]})
         hist.gradient.showTicks(False)
-        
+
         img.setImage(Sxx)
         img.scale(t[-1]/np.size(Sxx, axis=1), f[-1]/np.size(Sxx, axis=0))
-        self.spectrogramData[channel].setLimits(xMin=0, xMax=t[-1], yMin=0, yMax=f[-1])
+        self.spectrogramData[channel].setLimits(
+            xMin=0, xMax=t[-1], yMin=0, yMax=f[-1])
         self.spectrogramData[channel].setLabel('bottom', "Time", units='s')
         self.spectrogramData[channel].setLabel('left', "Frequency", units='Hz')
 
     def generatePDF(self):
         images = [0, 0, 0]
         rows = 0
-        xmax = [self.pointsToAppend[0], self.pointsToAppend[1], self.pointsToAppend[2]]
+        xmax = [self.pointsToAppend[0],
+                self.pointsToAppend[1], self.pointsToAppend[2]]
         for channel in range(3):
             if self.y[channel]:
                 images[channel] = 1
@@ -224,17 +249,19 @@ class MainWindow(qtw.QMainWindow):
                 self.hide(channel)
 
         if not rows:
-            qtw.QMessageBox.information(self, 'failed', 'You have to input a signal first')
+            qtw.QMessageBox.information(
+                self, 'failed', 'You have to input a signal first')
             return
-        outFile = qtw.QFileDialog.getSaveFileName(None, 'Load Signal', './', "Document(*.pdf)")
+        outFile = qtw.QFileDialog.getSaveFileName(
+            None, 'Load Signal', './', "Document(*.pdf)")
         report = PdfPages(outFile[0])
         fig = plt.figure(figsize=(12, 16))
-        G = GridSpec(2,1)
+        G = GridSpec(2, 1)
         for channel in range(3):
             if images[channel]:
                 fig = plt.figure(figsize=(12, 16))
-                G = GridSpec(2,1)
-                axes1 = plt.subplot(G[0,0])
+                G = GridSpec(2, 1)
+                axes1 = plt.subplot(G[0, 0])
                 self.getFigure(axes1, channel, xmax[channel])
                 axes2 = plt.subplot(G[1, 0])
                 self.getSpectrogram(axes2, channel)
@@ -248,7 +275,7 @@ class MainWindow(qtw.QMainWindow):
             xmin = xmax - xRange
         else:
             xmin = 0
-        
+
         fig.plot(self.x[channel][xmin:xmax], self.y[channel][xmin:xmax])
         fig.set_xlabel('time (sec)')
         fig.set_ylabel('amplitude (v)')
